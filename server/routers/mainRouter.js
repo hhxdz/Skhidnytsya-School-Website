df76033702919bf2ed622918bd9c73024e61221c
Пронь Олена <pronivanka16@gmail.com>
2026-09-10 20:37:49 +0300

    refactor: restructure server and add routes

--- server/routers/mainRouter.js
+++ server/routers/mainRouter.js
@@ -8,9 +8,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 
 mainRouter.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../../public', 'main.html'))
+    res.sendFile(path.join(__dirname, '../../pages', 'main.html'))
 })
 
+mainRouter.get('/teachers', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../../pages', 'teachers.html'))
+})
+
+mainRouter.get('/administration', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../../pages', 'administration.html'))
+})
+
+mainRouter.get('/classes', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../../pages', 'classes.html'))
+})
 
 
 
--- server/server.js
+++ server/server.js
@@ -16,9 +16,6 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-// app.get('/', (req, res)=>{
-//     res.sendFile(path.join(__dirname, '../public', 'main.html'));
-// })
 app.use('/', mainRouter)
 
 app.listen(PORT, ()=>{
